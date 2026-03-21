4de454d3f51d4614d7fac21076e28b60e9f66f4f
Andre1qaz <andresaragih932@gmail.com>
2026-03-21 11:13:18 +0700

    update

--- kampus-manager-be/src/main/kotlin/org/delcom/kampusmanager/util/AppException.kt
+++
@@ -1,3 +0,0 @@
-package org.delcom.kampusmanager.util
-
-class AppException(val code: Int, override val message: String) : Exception(message)
--- kampus-manager-be/src/main/kotlin/org/delcom/kampusmanager/util/Security.kt
+++
@@ -1,8 +0,0 @@
-package org.delcom.kampusmanager.util
-
-import org.mindrot.jbcrypt.BCrypt
-
-object Security {
-    fun hashPassword(plain: String): String = BCrypt.hashpw(plain, BCrypt.gensalt(12))
-    fun verifyPassword(plain: String, hashed: String): Boolean = BCrypt.checkpw(plain, hashed)
-}
