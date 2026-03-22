f71f29452f563babf819797db4cf6e71cdda80ce
Andre1qaz <andresaragih932@gmail.com>
2026-03-22 14:40:58 +0700

    update

--- src/main/kotlin/org/delcom/kampusmanager/config/JwtConfig.kt
+++ src/main/kotlin/org/delcom/kampusmanager/config/JwtConfig.kt
@@ -1,9 +1,9 @@
 package org.delcom.kampusmanager.config
 
 object JwtConfig {
-    const val AUTH_NAME = "kampus-jwt"
-    const val REALM     = "kampus-manager"
-    const val ISSUER    = "kampus-manager-app"
-    const val AUDIENCE  = "kampus-manager-user"
+    const val AUTH_NAME  = "kampus-jwt"
+    const val REALM      = "kampus-manager"
+    const val ISSUER     = "delcom-app"       // ← ubah dari "kampus-manager-app"
+    const val AUDIENCE   = "delcom-user"      // ← ubah dari "kampus-manager-user"
     const val EXPIRES_MS = 60L * 60 * 1000   // 1 jam
 }
